19d5f582592423632258fa7bc3c81c3caf7d341a
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-03 00:10:16 +0200

    fp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<NotFound />} />
+        <Route path="/notfound" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
