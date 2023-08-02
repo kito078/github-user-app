590fa5d716cce7ca446a402708cba874135e9861
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-02 22:40:15 +0200

    e

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          {/* <Link to="/" className="text-lg font-bold align-middle"> */}
+          GithubUser
+          {/* </Link> */}
+        </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            {/* <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              Home
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              About
+            </Link> */}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
