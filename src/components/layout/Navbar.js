09cfe03d4101cd9a04117de491b0f828c89eeb23
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-03 00:11:19 +0200

    d

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -6,19 +7,19 @@ function Navbar() {
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
           {/* <FaGithub className="inline pr-2 text-3xl" /> */}
-          {/* <Link to="/" className="text-lg font-bold align-middle"> */}
-          GithubUser
-          {/* </Link> */}
+          <Link to="/" className="text-lg font-bold align-middle">
+            GithubUser
+          </Link>
         </div>
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            {/* <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
               Home
             </Link>
             <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
               About
-            </Link> */}
+            </Link>
           </div>
         </div>
       </div>
