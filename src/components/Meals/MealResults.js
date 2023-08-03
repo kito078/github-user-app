1319e616d3c0437f54dbfd73e7a69db665caeaf4
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-03 15:22:37 +0200

    s

--- src/components/Meals/MealResults.js
+++ src/components/Meals/MealResults.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function MealResults() {
-  return <div>results</div>;
+  return (
+    <div className="mx-4 ">
+      <h3 className="text-4xl font-bold">Unreal places to discove</h3>
+      <span>Wednesday, Sep 21</span>
+    </div>
+  );
 }
 
 export default MealResults;
