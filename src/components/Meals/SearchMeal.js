44fab314dcc37f3c490a1d9cf4d25a0e2e10254d
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-03 11:12:50 +0200

    dd

--- src/components/Meals/SearchMeal.js
+++ src/components/Meals/SearchMeal.js
@@ -39,7 +39,7 @@ function SearchMeal() {
         <input
           type="search"
           id="default-search"
-          class="block w-full p-4 pl-10 text-sm text-gray-900 border 
+          class="block w-full p-2 pl-10 text-sm text-gray-900 border 
   border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 
   focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
   dark:placeholder-gray-400 dark:text-white focus:outline-none
