0de3036c66555e25c74da3934e2baf6494284087
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-03 15:28:33 +0200

    ss

--- src/components/Meals/MealResults.js
+++ src/components/Meals/MealResults.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MealResults() {
   return (
     <div className="mx-4 ">
-      <h3 className="text-4xl font-bold">Unreal places to discove</h3>
-      <span>Wednesday, Sep 21</span>
+      <h3 className="text-4xl font-bold mb-3">Unreal places to discover</h3>
+      <span className="text-2xl ml-1">Wednesday, Sep 21</span>
+      <div>
+        <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+          Good Reviews{" "}
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MealResults from "../components/Meals/MealResults";
 
 function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      <MealResults />
+    </div>
+  );
 }
 
 export default Home;
