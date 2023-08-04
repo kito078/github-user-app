ab405d821a08fb1c3962e62e7b594e86e689847a
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-04 10:48:54 +0200

    s

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -8,27 +8,50 @@ function MealItem({ meal }) {
   console.log(meal);
   const { strMeal, strMealThumb } = meal;
   return (
-    <div>
-      <div className="mt-6 h-96 ">
+    // <div>
+    //   <div className="mt-6 h-96 ">
+    //     <img
+    //       className="w-full h-96 object-cover rounded-[32px] "
+    //       src={strMealThumb}
+    //       alt=""
+    //     />
+    //     <div className="flex justify-between items-center mx-3">
+    //       <div>
+    //         <h3 className="text-2xl font-bold mb-3">Clumsies Bar</h3>
+    //         <div className="flex justify-items-center items-center">
+    //           <span>
+    //             <FaCreativeCommonsPdAlt className="text-lg" />
+    //           </span>
+    //           <span className="ml-2">8 Awe SW Colgory</span>
+    //         </div>
+    //       </div>
+    //       <div>
+    //         <div className="w-10 h-16 bg-white rounded-[32px] flex items-center justify-center">
+    //           <AiTwotoneHeart className="text-3xl text-orange-500" />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="mb-32">
+      <div className="mt-6 h-96 relative">
         <img
-          className="w-full h-96 object-cover rounded-[32px] "
+          className="w-full h-96 object-cover rounded-[32px]"
           src={strMealThumb}
           alt=""
         />
-        <div className="flex justify-items-center items-center">
-          <div>
-            <h3 className="text-2xl font-bold mb-3">Clumsies Bar</h3>
-            <div className="flex justify-items-center items-center">
-              <span>
-                <FaCreativeCommonsPdAlt className="text-lg" />
-              </span>
-              <span className="ml-2">8 Awe SW Colgory</span>
-            </div>
+        <div className="absolute bottom-0 left-0 w-full p-3 mb-4 text-white">
+          <h3 className="text-2xl font-bold mb-3">Clumsies Bar</h3>
+          <div className="flex justify-items-center items-center">
+            <span>
+              <FaCreativeCommonsPdAlt className="text-lg" />
+            </span>
+            <span className="ml-2">8 Awe SW Colgory</span>
           </div>
-          <div>
-            <div className="w-10 h-16 bg-white rounded-[32px]">
-              <AiTwotoneHeart className="text-3xl" />
-            </div>
+        </div>
+        <div className="absolute bottom-0 right-0 mb-4 p-3">
+          <div className="w-10 h-16 bg-white rounded-[32px] flex items-center justify-center">
+            <AiTwotoneHeart className="text-3xl text-orange-500" />
           </div>
         </div>
       </div>
