759707d3648fcf1902ed55e3cf83c5c4ca9594c5
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-08 10:55:49 +0200

    s

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return <div className="container-one">NOTFOUND</div>;
+  return (
+    <div className="container-one">
+      <div className="hero">
+        <div className="max-w-lg">
+          <h3 className="text-white text-8xl font-bold mb-8">Oops!</h3>
+          <p className="text-5xl mb-8 text-white">404 - Page notfound</p>
+          <Link
+            to="/"
+            class="rounded-full text-white bg-gray-500 py-3 px-7 font-bold"
+          >
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
