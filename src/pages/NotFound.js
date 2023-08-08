55de40eab6e587cb9303167842d46a9e45210aee
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-08 11:00:09 +0200

    s

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <div className="container-one">
-      <div className="hero">
-        <div className="max-w-lg">
-          <h3 className="text-white text-8xl font-bold mb-8">Oops!</h3>
-          <p className="text-5xl mb-8 text-white">404 - Page notfound</p>
-          <Link
-            to="/"
-            class="rounded-full text-white bg-gray-500 py-3 px-7 font-bold"
-          >
-            Back To Home
-          </Link>
+      <div className="hero flex flex-col items-center justify-items-center">
+        <div className="text-center hero-content">
+          <div className="max-w-lg">
+            <h3 className="text-white text-8xl font-bold mb-8">Oops!</h3>
+            <p className="text-5xl mb-8 text-white">404 - Page notfound</p>
+            <Link
+              to="/"
+              class="rounded-full text-white bg-gray-700 hover:bg-gray-600 py-3 px-7 font-bold"
+            >
+              Back To Home
+            </Link>
+          </div>
         </div>
       </div>
     </div>
