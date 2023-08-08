edf6e1fcf4a2fe15e1fff5bb6e9eb9a62cd3b150
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-08 09:34:04 +0200

    ss

--- src/context/MealContext.js
+++ src/context/MealContext.js
@@ -1,3 +1,47 @@
+// import { createContext, useEffect, useReducer } from "react";
+// import mealReducer from "./MealReducer";
+// import axios from "axios";
+
+// const MealContext = createContext();
+
+// export const MealProvider = ({ children }) => {
+//   const initialState = {
+//     meals: [],
+//   };
+
+//   const [state, dispatch] = useReducer(mealReducer, initialState);
+
+//   //fetch meal
+//   useEffect(() => {
+//     fetchMeal();
+//   }, []);
+
+//   const fetchMeal = async () => {
+//     try {
+//       const response = await axios.get(
+//         `${process.env.REACT_APP_URL}/search.php?s=Arrabiata`
+//       );
+//       //setMeals(response.data.meals);
+//       const items = response.data.meals;
+//       console.log(items);
+//       dispatch({
+//         type: "GET_MEALS",
+//         payload: items,
+//       });
+//     } catch (error) {
+//       console.error("error in fetching ", error.message);
+//     }
+//   };
+
+//   return (
+//     <MealContext.Provider value={{ meals: state.meals, fetchMeal }}>
+//       {children}
+//     </MealContext.Provider>
+//   );
+// };
+
+// export default MealContext;
+
 import { createContext, useEffect, useReducer } from "react";
 import mealReducer from "./MealReducer";
 import axios from "axios";
@@ -11,23 +55,22 @@ export const MealProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(mealReducer, initialState);
 
-  //fetch meal
   useEffect(() => {
     fetchMeal();
   }, []);
 
   const fetchMeal = async () => {
     try {
-      const response = await axios.get(`${process.env}/search.php?s=Arrabiata`);
-      //setMeals(response.data.meals);
+      const response = await axios.get(
+        `${process.env.REACT_APP_URL}/search.php?s=Arrabiata`
+      );
       const items = response.data.meals;
-      console.log(items);
       dispatch({
         type: "GET_MEALS",
         payload: items,
       });
     } catch (error) {
-      console.error("error in fetching ", error.message);
+      console.error("Error in fetching: ", error.message);
     }
   };
 
