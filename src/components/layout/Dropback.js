27c0eb1a06154f208cfc590688f7e88aad395f9b
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-16 12:12:26 +0200

    ff

--- src/components/layout/Dropback.js
+++ src/components/layout/Dropback.js
@@ -1,20 +1,76 @@
 import React, { useContext, useState } from "react";
 import MealContext from "../../context/MealContext";
+import { useNavigate } from "react-router-dom";
 
 function Dropback() {
-  const [text, setText] = useState("chicken");
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
+
   const { fetchMeal } = useContext(MealContext);
 
-  const handleClick = (text) => {
-    console.log(text);
-    fetchMeal(text);
+  const options = ["rice", "beef", "chicken"];
+
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
   };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    fetchMeal(text);
+    navigate("/");
+  };
+
   return (
-    <div>
-      <button onClick={() => handleClick} className="blue-500 text-white">
-        click
-      </button>
-    </div>
+    <form onSubmit={onSubmitHandler} className="w-96">
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium sr-only text-white"
+      >
+        Search Meals
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 ml-4 text-gray-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+
+        <select
+          onChange={onChangeHandler}
+          value={text}
+          id="default-search"
+          className="block w-full ml-2 p-3 pl-10 text-sm border rounded-[21px] bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:outline-none focus:ring-blue-500"
+          required
+        >
+          <option value="">Select an option...</option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+
+        <button
+          type="submit"
+          className="hidden text-white absolute right-2.5 bottom-2.5 font-medium rounded-lg text-sm px-4 py-2 bg-sky-600 hover:bg-sky-700 focus:ring-sky-800"
+        >
+          go
+        </button>
+      </div>
+    </form>
   );
 }
 
