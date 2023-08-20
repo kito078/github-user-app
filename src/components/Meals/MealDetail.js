9f77a2a11388bd7183b37745588f4502f7d0c5cf
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-21 00:34:25 +0200

    s

--- src/components/Meals/MealDetail.js
+++ src/components/Meals/MealDetail.js
@@ -3,19 +3,22 @@ import React from "react";
 function MealDetail({ item }) {
   const { strCategory, strMealThumb } = item;
   return (
-    <div>
+    <div className="container-one">
+      <img className="w-96 rounded-xl" src={strMealThumb} alt="" />
       <h1 className="text-6xl font-semibold text-gray-200">{strCategory}</h1>
-      <img src={strMealThumb} alt="" />
-      <div className="flex justify-center items-center h-screen">
+    </div>
+  );
+}
+
+export default MealDetail;
+
+{
+  /* <div className="flex justify-center items-center h-screen">
         <iframe
           className="w-3/4 h-3/4"
           src="https://www.youtube.com/embed/kqEfk801E94"
           title="YouTube Video"
           allowFullScreen
         ></iframe>
-      </div>
-    </div>
-  );
+      </div> */
 }
-
-export default MealDetail;
