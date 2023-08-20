64c1fdf7062f341dda57fa8bf46fd428696ec13b
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-20 09:01:21 +0200

    S

--- src/components/layout/Popular.js
+++ src/components/layout/Popular.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function Popular() {
   return (
     <div className="bg-decoration-zinc-900">
-      <h3 className="text-4xl md:text-4xl text-gray-300 font-bold mb-3 leading-normal">
+      <h3 className="text-4xl md:text-6xl text-gray-300 text-center font-bold mb-3 leading-normal">
         Unreal places to discover
       </h3>
       <span className="text-xl ml-1 text-gray-400">Wednesday, Sep 21</span>
-      <div className="mt-9 flex ">
+      {/* <div className="mt-9 flex ">
         <Link
           to="/"
           className="btn btn-ghost text-1xl bg-gray-700 hover:bg-gray-600 py-3 text-gray-300 rounded-[32px] px-6 mr-2"
@@ -21,7 +21,7 @@ function Popular() {
         >
           Meals{" "}
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }
