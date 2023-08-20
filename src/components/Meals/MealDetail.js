833263d3a6a03832b815f8159890350da5fdc48b
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-20 08:41:02 +0200

    x

--- src/components/Meals/MealDetail.js
+++ src/components/Meals/MealDetail.js
@@ -3,12 +3,16 @@ import React from "react";
 function MealDetail({ item }) {
   const { strCategory, strMealThumb } = item;
   return (
-    <div>
-      <h1 className="text-8xl">{strCategory}</h1>
-      <img src={strMealThumb} alt="" />
-      <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold mb-6">{strCategory}</h1>
+      <img
+        src={strMealThumb}
+        alt=""
+        className="rounded-lg shadow-md w-80 h-80 object-cover mb-6"
+      />
+      <div className="w-3/4 h-3/4 bg-white rounded-lg shadow-md overflow-hidden">
         <iframe
-          className="w-3/4 h-3/4"
+          className="w-full h-full"
           src="https://www.youtube.com/embed/kqEfk801E94"
           title="YouTube Video"
           allowFullScreen
@@ -19,3 +23,25 @@ function MealDetail({ item }) {
 }
 
 export default MealDetail;
+
+// import React from "react";
+
+// function MealDetail({ item }) {
+//   const { strCategory, strMealThumb } = item;
+//   return (
+//     <div>
+//       <h1 className="text-8xl">{strCategory}</h1>
+//       <img src={strMealThumb} alt="" />
+//       <div className="flex justify-center items-center h-screen">
+//         <iframe
+//           className="w-3/4 h-3/4"
+//           src="https://www.youtube.com/embed/kqEfk801E94"
+//           title="YouTube Video"
+//           allowFullScreen
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MealDetail;
--- src/components/layout/Dropback.js
+++ src/components/layout/Dropback.js
@@ -1,3 +1,71 @@
+// import React, { useContext, useState } from "react";
+// import MealContext from "../../context/MealContext";
+
+// function Dropback() {
+//   const [text, setText] = useState("chicken");
+//   const [isOpen, setIsOpen] = useState(false);
+//   const { fetchMeal } = useContext(MealContext);
+
+//   const options = ["breakfast", "pork", "cheese", "Vegetarian"];
+
+//   const handleDropdownChange = (option) => {
+//     setText(option);
+//     fetchMeal(option);
+//     setIsOpen(false); // Close the dropdown after selecting an option
+//   };
+
+//   const toggleHandler = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div className="relative inline-block">
+//       <button
+//         onClick={toggleHandler}
+//         className="block hover:text-softRed flex align-center justify-items-center"
+//       >
+//         <span className="text-gray-400">Category</span>{" "}
+//         <svg
+//           className="w-4 h-4 ml-2.5 mt-1 text-gray-400"
+//           aria-hidden="true"
+//           xmlns="http://www.w3.org/2000/svg"
+//           fill="none"
+//           viewBox="0 0 10 6"
+//         >
+//           <path
+//             stroke="currentColor"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//             strokeWidth="2"
+//             d="m1 1 4 4 4-4"
+//           />
+//         </svg>
+//       </button>
+
+//       {/* Dropdown menu */}
+//       {isOpen && (
+//         <ul
+//           className="absolute z-10 bg-gray-700 divide-y divide-gray-100 rounded-lg shadow w-44"
+//           aria-labelledby="dropdownDefaultButton"
+//         >
+//           {options.map((option, index) => (
+//             <li key={index}>
+//               <button
+//                 className="block px-4 py-2 hover:bg-gray-600 hover:text-white w-full text-left"
+//                 onClick={() => handleDropdownChange(option)}
+//               >
+//                 {option}
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Dropback;
+
 import React, { useContext, useEffect, useState } from "react";
 import MealContext from "../../context/MealContext";
 
@@ -11,11 +79,16 @@ function Dropback() {
   const handleDropdownChange = (text) => {
     setText(text);
     fetchMeal(text);
+    setIsOpen(false);
+  };
+
+  const toggleHandler = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <div>
-      <div className="w-full py-3 text-center ">
+      <div onClick={toggleHandler} className="w-full py-3 text-center ">
         <a
           href="#about"
           id="dropdownDefaultButton"
@@ -42,96 +115,33 @@ function Dropback() {
       </div>
 
       {/* Dropdown menu  */}
-      <div
-        id="dropdown"
-        className="z-10 hidden absolute bg-gray-700 divide-y divide-gray-100 rounded-lg shadow w-44"
-      >
-        <ul
-          className="py-2 text-sm font-semibold text-gray-200"
-          aria-labelledby="dropdownDefaultButton"
+      {isOpen && (
+        <div
+          id="dropdown"
+          className="z-10  absolute bg-gray-700 divide-y divide-gray-100 rounded-lg shadow w-44"
         >
-          {options.map((option, index) => (
-            <li key={index}>
-              <a
-                href="#"
-                className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
-                onClick={() => handleDropdownChange(option)}
-              >
-                {option}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+          <ul
+            className="py-2 text-sm font-semibold text-gray-200 "
+            aria-labelledby="dropdownDefaultButton"
+          >
+            {options.map((option, index) => (
+              <li key={index}>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-600 hover:text-white"
+                  onClick={() => handleDropdownChange(option)}
+                >
+                  {option}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Dropback;
 
-// import React, { useState } from "react";
-
-// function Dropback() {
-//   const [selectedOption, setSelectedOption] = useState("");
-
-//   const options = ["rice", "meet", "beef", "chicken"];
-
-//   const handleDropdownChange = (e) => {
-//     console.log(e.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <div onChange={handleDropdownChange} className="w-full py-3 text-center ">
-//         <a
-//           href="#about"
-//           id="dropdownDefaultButton"
-//           data-dropdown-toggle="dropdown"
-//           className="block hover:text-softRed flex align-center justify-items-center"
-//         >
-//           <span className="text-gray-400">Category</span>{" "}
-//           <svg
-//             class="w-4 h-4 ml-2.5 mt-1 text-gray-400"
-//             aria-hidden="true"
-//             xmlns="http://www.w3.org/2000/svg"
-//             fill="none"
-//             viewBox="0 0 10 6"
-//           >
-//             <path
-//               stroke="currentColor"
-//               stroke-linecap="round"
-//               stroke-linejoin="round"
-//               stroke-width="2"
-//               d="m1 1 4 4 4-4"
-//             />
-//           </svg>
-//         </a>
-//       </div>
-
-//       {/* Dropdown menu  */}
-//       <div
-//         id="dropdown"
-//         class="z-10 absolute bg-gray-700 divide-y divide-gray-100 rounded-lg shadow w-44 "
-//       >
-//         <ul
-//           class="py-2 text-sm text-gray-200"
-//           aria-labelledby="dropdownDefaultButton"
-//         >
-//           {options.map((option, index) => (
-//             <li>
-//               <a
-//                 key={index}
-//                 href="#"
-//                 class="block px-4 py-2  hover:bg-gray-600 hover:text-white"
-//               >
-//                 {option}
-//               </a>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Dropback;
+//www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
