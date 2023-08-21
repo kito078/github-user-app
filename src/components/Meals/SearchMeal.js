721004a94a33f075e03fa182cc5368f158a16090
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-21 15:04:52 +0200

    s

--- src/components/Meals/SearchMeal.js
+++ src/components/Meals/SearchMeal.js
@@ -29,7 +29,7 @@ function SearchMeal() {
       ></label>
       <div class="relative">
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg
+          {/* <svg
             class="w-5 h-5 ml-4  text-gray-300"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +43,7 @@ function SearchMeal() {
               stroke-width="2"
               d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
             />
-          </svg>
+          </svg> */}
         </div>
 
         <div>
@@ -52,7 +52,7 @@ function SearchMeal() {
             value={text}
             type="search"
             id="default-search"
-            class="block w-full  ml-2 p-5 pl-12 text-sm  border 
+            class=" w-64  md:w-full   p-5 pl-12 text-sm  border 
              rounded-[30px]  
              bg-gray-700 border-gray-600
             placeholder-gray-400 text-white focus:outline-none 
