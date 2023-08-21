ce52b61efc9e56f2ffc55e597f7305896c2a4bf1
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-08-21 23:39:59 +0200

    x

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MealResults from "../components/Meals/MealResults";
 
 function Home() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="container-one">
       <MealResults />
